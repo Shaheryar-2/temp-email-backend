7a8093cba4fb4c1e67327d8f24bc3df841444cfd
Shaheryar-2 <sharymirza457@gmail.com>
2025-08-24 22:50:56 +0500

    api url parsing corrected

--- app.js
+++ app.js
@@ -12,7 +12,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://www.tempmailbox.org',
+  credentials: true
+}));
 app.use(bodyParser.json());
 
 // Database connection
